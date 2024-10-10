5f3b240ecf128c7d84f7f4a3ca2909a1daf1234b
Volodymyr <vova.senyk@gmail.com>
2024-10-10 20:55:36 +0300

    Refactor Header.js to use Link component for logo link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import logo from '../logo.png'
+import { Link } from 'react-router-dom'
 
 import Cart from './Cart'
 
@@ -6,9 +7,9 @@ export default function Header() {
   return (
     <header className="App-header border-b border-neutral-200 bg-white py-4">
       <div className="container relative flex items-center lg:justify-center">
-        <a href="/" className="block max-w-[75%]">
+        <Link href="/" className="block max-w-[75%]">
           <img src={logo} alt="logo" />
-        </a>
+        </Link>
         <Cart />
       </div>
     </header>
